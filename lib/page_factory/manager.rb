bb1157a1003e7ea9b3d4225bffe2fe9bc0dce8c7
Josh French <josh@digitalpulp.com>
2010-03-26 19:50:29 -0400

    Manager.prune! takes a class argument
    This is some FINE block usage, btw.

--- lib/page_factory/manager.rb
+++ lib/page_factory/manager.rb
@@ -1,8 +1,12 @@
 class PageFactory
   class Manager
     class << self
-      def prune!
-        PageFactory.descendants.each do |factory|
+
+      def prune!(klass=nil)
+        by_factory = lambda do |descendant|
+          klass.nil? ? true : descendant.name == klass.to_s.camelcase
+        end
+        PageFactory.descendants.select(&by_factory).each do |factory|
           parts = PagePart.find :all, :include => :page,
                   :conditions => ['pages.page_factory = :factory AND page_parts.name NOT IN (:parts)',
                                   {:factory => factory.name, :parts => factory.parts.map(&:name)}
--- spec/lib/manager_spec.rb
+++ spec/lib/manager_spec.rb
@@ -4,6 +4,7 @@ describe PageFactory::Manager do
   dataset do
     create_record :page, :managed, :title => 'managed', :slug => 'managed', :breadcrumb => 'managed', :page_factory => 'ManagedPageFactory'
     create_record :page, :plain, :title => 'plain', :slug => 'plain', :breadcrumb => 'plain'
+    create_record :page, :other, :title => 'other', :slug => 'other', :breadcrumb => 'other'
     create_record :page_part, :existing, :name => 'existing'
     create_record :page_part, :old, :name => 'old'
     create_record :page_part, :new, :name => 'new'
@@ -15,8 +16,8 @@ describe PageFactory::Manager do
   end
 
   before do
-    @managed, @plain, @existing, @old, @new =
-    pages(:managed), pages(:plain), page_parts(:existing), page_parts(:old), page_parts(:new)
+    @managed, @plain, @other, @existing, @old, @new =
+    pages(:managed), pages(:plain), pages(:other), page_parts(:existing), page_parts(:old), page_parts(:new)
   end
 
   describe "#prune!" do
@@ -39,14 +40,29 @@ describe PageFactory::Manager do
       @plain.reload.parts.should include(@old)
     end
 
-    it "should operate on a single factory"
+    it "should operate on a single factory" do
+      @managed.parts.concat [@existing.clone, clone = @old.clone]
+      @other.parts.concat [@existing, @old]
+      PageFactory::Manager.prune! :ManagedPageFactory
+      @managed.reload.parts.should_not include(clone)
+      @other.reload.parts.should include(@old)
+    end
   end
 
-  describe "#update!" do
+  describe "#update_parts!" do
     it "should add missing parts"
     it "should not override matching parts"
     it "should override parts whose classes don't match"
     it "should operate on a single factory"
   end
   
+  describe "#update_layouts!" do
+    it "should change page layout to match factory"
+    it "should operate on a single factory"
+  end
+
+  describe "#update_classes!" do
+    it "should change page class to match factory"
+    it "should operate on a single factory"
+  end
 end
