4279759406b7c704f9ad1d41eadd116fd8fb5ddd
Josh French <josh@digitalpulp.com>
2010-04-13 16:14:16 -0400

    Add JS for single-factory redirects

---
+++ public/javascripts/admin/pagefactory.js
@@ -0,0 +1,17 @@
+Popup.PageFactoryTriggerBehavior = Behavior.create(Popup.TriggerBehavior, {
+  initialize: function() {
+    this.window = new Popup.PageFactoryWindow(this.element.href);
+  },
+});
+
+Popup.PageFactoryWindow = Class.create(Popup.AjaxWindow, {
+  show: function($super) {
+      new Ajax.Updater(this.content, this.url, {asynchronous: false, method: "get", evalScripts: true,
+                         onComplete: function(response) { (response.getHeader('Location') ? window.location.href = response.getHeader('Location') : $super() )}
+                       })
+  },
+});
+
+Event.addBehavior({
+  'a.popup-factory': Popup.PageFactoryTriggerBehavior(),
+})
